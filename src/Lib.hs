10eb5ad775377cf5677859b235b6bcf04dc3f5dc
Jeffrey Dwyer <Jeffrey Dwyer>
2017-03-10 18:17:37 -0800

    Extracted data from floor1.json

--- src/Lib.hs
+++ src/Lib.hs
@@ -17,6 +17,8 @@ import qualified Data.HashMap.Lazy as HM
 import qualified Data.Hashable as H
 import qualified Data.Vector as V
 import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import qualified System.Directory as D
 import Control.Monad.Trans.Maybe
 import Control.Monad.IO.Class
 import Control.Monad
@@ -24,6 +26,7 @@ import Data.Aeson.Encode.Pretty
 import Data.Time.Clock
 import Data.List
 import Data.Monoid
+import Control.Arrow
 
 data Employee = Employee { lastName :: String
                          , firstName :: String
@@ -68,14 +71,30 @@ loadData filepath = decode <$> BL.readFile filepath
 mergeHashMap :: (Eq k, H.Hashable k) => [HM.HashMap k v] -> HM.HashMap k [v]
 mergeHashMap = foldl' (HM.unionWith (++)) mempty . fmap (HM.map (:[]))
 
+validateFileName = T.replace ":" " "
+                 . T.replace "\\" " "
+                 . T.replace "/" " "
+
+loggingDir :: T.Text
+loggingDir = "data-logs/"
+
+logDataToFile :: T.Text -> [Value] -> IO ()
+logDataToFile key dt = do
+    print $ T.concat ["Logging data to ", path]
+    mapM_ (TIO.appendFile (T.unpack path) . format) dt
+    where path =  T.concat [loggingDir, T.concat [validateFileName key, ".txt"]]
+          format v = (T.snoc (v ^. _String) '\n')
+
 someFunc :: IO ()
 someFunc = do
   floor1 <- loadFloorData "data/floor1-MC2.json"
   employees <- loadData @[Employee] "data/employees.json"
   proxOuts <- loadData @[[ProxOut]] "data/proxOut-MC2.json"
+  D.createDirectoryIfMissing False "data-logs"
   -- I need to generalize this to all of the keys
-  maybeIO (floor1) $ mapM_ (appendFile "output.txt" . show)
-                   . HM.lookup "F_1_Z_7: Thermostat Cooling Setpoint"
+  maybeIO (floor1) $ mapM_ (uncurry logDataToFile)
+                   . HM.toList
+
  
 
     
